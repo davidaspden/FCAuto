08ce4e3faa1d5a5047dfc2033808a50f1b2034b4
davidaspden <david.aspden@gmail.com>
2024-11-04 14:15:17 +0000

    Style changes. Control barcodes, sequential QS printing.

--- script.js
+++ script.js
@@ -110,21 +110,18 @@ function printAnylabel(b, t, d,n) {
   );
 }
 
-function printSpecial(t,d,n) {
+function printTab() {
   var badge = 1;
   getStatus(
-    
     "http://localhost:5965/printer?action=print&type=barcode&" +
-      "data=" +
-      encodeURIComponent(asciihex(t)) +
-      "&text=" +
-      encodeURIComponent(asciihex(t)) +
+      "data=654964&text=" +
+      encodeURIComponent(asciihex("Single Tab")) +
       "&quantity=" +
-      encodeURIComponent(parseInt(n)) +
+      encodeURIComponent(1) +
       "&badgeid=" +
-      encodeURIComponent(parseInt(badge)) +
+      encodeURIComponent(parseInt(1)) +
       "&desc=" +
-      encodeURIComponent(asciihex(d)) +
+      encodeURIComponent(asciihex("Single Tab")) +
       "&seq=" +
       encodeURIComponent(genId()),
     function () {},
@@ -132,25 +129,56 @@ function printSpecial(t,d,n) {
   );
 }
 
-function printQS(){
-  for(let k=1; k<11; k++){
-    getStatus(
-      "http://localhost:5965/printer?action=print&type=barcode&" +
-        "data=" +
-        encodeURIComponent(asciihex((k).toString())) +
-        "&text=" +
-        encodeURIComponent(asciihex((k).toString())) +
-        "&quantity=" +
-        encodeURIComponent(parseInt(2)) +
-        "&badgeid=" +
-        encodeURIComponent(parseInt(0)) +
-        "&desc=" +
-        encodeURIComponent(asciihex("------")) +
-        "&seq=" +
-        encodeURIComponent(genId()),
-      function () {},
-      printFailed
-    );
+function printDoubleTab() {
+  var badge = 1;
+  getStatus(
+    "http://localhost:5965/printer?action=print&type=barcode&" +
+      "data=65494964&text=" +
+      encodeURIComponent(asciihex("Double Tab")) +
+      "&quantity=" +
+      encodeURIComponent(1) +
+      "&badgeid=1&desc=" +
+      encodeURIComponent(asciihex("Double Tab")) +
+      "&seq=" +
+      encodeURIComponent(genId()),
+    function () {},
+    printFailed
+  );
+}
+
+// Updated printQS function to handle requests sequentially
+async function printQS() {
+  for (let k = 1; k < 11; k++) {
+    try {
+      await new Promise((resolve, reject) => {
+        getStatus(
+          "http://localhost:5965/printer?action=print&type=barcode&" +
+            "data=" +
+            encodeURIComponent(asciihex(k.toString())) +
+            "&text=" +
+            encodeURIComponent(asciihex(k.toString())) +
+            "&quantity=" +
+            encodeURIComponent(2) +
+            "&badgeid=" +
+            encodeURIComponent(0) +
+            "&desc=" +
+            encodeURIComponent(asciihex("------")) +
+            "&seq=" +
+            encodeURIComponent(genId()),
+          () => {
+            resolve(); // Call resolve to proceed to the next request
+          },
+          () => {
+            printFailed(); // Trigger the failure callback
+            reject(); // Stop further requests on failure
+          }
+        );
+      });
+    } catch (error) {
+      console.error("Error in printQS: Request failed");
+      break; // Stop further requests if any fail
+    }
   }
 }
+
 
